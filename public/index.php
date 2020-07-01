5bef253bba7c0fde105a12a3caa53052953090be
Reinhard Westermann <reinhard.westermann@kartenmacherei.de>
2020-07-01 15:16:01 +0200

    add placeholder elements

--- public/index.php
+++ public/index.php
@@ -5,6 +5,33 @@
     <title>mit-marion.de</title>
 </head>
 <body>
-
+<header>
+    <div>logo</div>
+    <div>burger</div>
+    <div>breadcrumb</div>
+</header>
+<article class="topic">
+    <section class="member">
+        <q class="mainquote">das müsste ich eigentlich viel häufiger machen!</q>
+        <img class="mainquote" src="images/dummy.png"/>
+        <q class="subquote">Ich spiele schon soo lange Volleyball, aber erst jetzt merke ich, was mir fehlt!</q>
+    </section>
+    <section class="marion">
+        <q>Sowas höre ich nicht selten! Da ist jemand schon sehr sportlich und plötzlich ist da der Ah-Ha Effekt.</q>
+        <ul>
+            <li>die Beine erholen sich viel schneller,</li>
+            <li>der Bewegungsumfang der Arme ist plötzlich viel größer</li>
+            <li>der Muskelkater bleibt aus</li>
+        </ul>
+        <p>Die Faszienübungen sind echte Allrounder!</p>
+    </section>
+    <a href="#">Jetzt zur Probe mitmachen</a>
+</article>
+<nav class="pager">
+    <ul>
+        <li><a href="#dem-regen-trotzen-und-noch-länger">dem-regen-trotzen-und-noch-länger</a></li>
+        <li><a href="#den-rücken-verrücken">den-rücken-verrücken</a></li>
+    </ul>
+</nav>
 </body>
 </html>
