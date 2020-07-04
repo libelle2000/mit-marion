63ee1275a12935737814404ae42f0fdb3fefba46
Reinhard Westermann <reinhard.westermann@kartenmacherei.de>
2020-07-04 18:03:54 +0200

    move media queries to the bottom adjustments for wider screens - add shadow to content on where margin left and right is added - let logo and burger be bigger

--- public/index.php
+++ public/index.php
@@ -8,63 +8,70 @@
     <script async="async" src='/js/main.js'></script>
 </head>
 <body>
-<header class="container-content">
-    <img class="logo" src="/images/logo.svg" alt="Logo mit-marion.de"/>
-    <nav class="corporate closed jsFlyoutWrapper jsFlyoutHoverToggle">
-        <img class="burger jsFlyoutTouchToggle jsFlyoutHoverToggle" src="/images/burger-menu.svg" alt="Menü Icon"/>
-        <ul class="flyout jsFlyoutHoverToggle">
-            <li><a href="#bar">meine Qualifikationen</a></li>
-            <li class="current"><a href="#bar">meine Qualifikationen</a></li>
-            <li><a href="#bar">über mich</a></li>
-            <li><a href="#bar">bar</a></li>
-            <li><a href="#bar">bar und noch viel viel mehr und noch viel viel mehr</a></li>
-            <li><a href="#bar">bar</a></li>
-            <li><a href="#bar">bar</a></li>
-        </ul>
-    </nav>
-    <nav class="stories closed jsFlyoutWrapper jsFlyoutHoverToggle">
-        <div class="breadcrumb jsFlyoutTouchToggle jsFlyoutHoverToggle">die-fazien-fetzen und noch viel viel mehr und noch viel viel mehr</div>
-        <ul class="flyout jsFlyoutHoverToggle">
-            <li class="breadcrumb current jsFlyoutTouchToggle">die-fazien-fetzen und noch viel viel mehr und noch viel viel mehr</li>
-            <li><a href="#foo">die-fazien-fetzen</a></li>
-            <li><a href="#bar">bar</a></li>
-            <li><a href="#bar">bar</a></li>
-            <li><a href="#bar">bar</a></li>
-            <li><a href="#bar">bar und noch viel viel mehr und noch viel viel mehr</a></li>
-            <li><a href="#bar">bar</a></li>
-            <li><a href="#bar">bar</a></li>
-        </ul>
-    </nav>
+<div class="content">
+    <header class="container-content">
+        <img class="logo" src="/images/logo.svg" alt="Logo mit-marion.de"/>
+        <nav class="corporate closed jsFlyoutWrapper jsFlyoutHoverToggle">
+            <img class="burger jsFlyoutTouchToggle jsFlyoutHoverToggle" src="/images/burger-menu.svg" alt="Menü Icon"/>
+            <ul class="flyout jsFlyoutHoverToggle">
+                <li><a href="#bar">meine Qualifikationen</a></li>
+                <li class="current"><a href="#bar">meine Qualifikationen</a></li>
+                <li><a href="#bar">über mich</a></li>
+                <li><a href="#bar">bar</a></li>
+                <li><a href="#bar">bar und noch viel viel mehr und noch viel viel mehr</a></li>
+                <li><a href="#bar">bar</a></li>
+                <li><a href="#bar">bar</a></li>
+            </ul>
+        </nav>
+        <nav class="stories closed jsFlyoutWrapper jsFlyoutHoverToggle">
+            <div class="breadcrumb jsFlyoutTouchToggle jsFlyoutHoverToggle">die-fazien-fetzen und noch viel viel mehr
+                und noch viel viel mehr
+            </div>
+            <ul class="flyout jsFlyoutHoverToggle">
+                <li class="breadcrumb current jsFlyoutTouchToggle">die-fazien-fetzen und noch viel viel mehr und noch
+                    viel viel mehr
+                </li>
+                <li><a href="#foo">die-fazien-fetzen</a></li>
+                <li><a href="#bar">bar</a></li>
+                <li><a href="#bar">bar</a></li>
+                <li><a href="#bar">bar</a></li>
+                <li><a href="#bar">bar und noch viel viel mehr und noch viel viel mehr</a></li>
+                <li><a href="#bar">bar</a></li>
+                <li><a href="#bar">bar</a></li>
+            </ul>
+        </nav>
 
-</header>
-<article class="container-content">
-    <section class="member">
-        <blockquote class="mainquote" style="background-image: url('/images/faszien-rolle.jpg'); background-position: right, bottom;">
-            <span>
-                "das müsste ich eigentlich viel häufiger machen!"
-            </span>
-        </blockquote>
-        <blockquote class="subquote">
-            "Ich spiele schon soo lange Volleyball, aber erst jetzt merke ich, was mir fehlt!"
-        </blockquote>
-    </section>
-    <section class="marion">
-        <blockquote class="floatedGroup">
-            <img class="photo floatLeft" src="/images/marion.jpg" alt="Trainerin Marion" />
-            "Sowas höre ich öfter! Da ist jemand schon sehr sportlich und plötzlich ist da der Ah-Ha Effekt."
-        </blockquote>
-        <ul>
-            <li>die Beine erholen sich viel schneller,</li>
-            <li>der Bewegungsumfang der Arme ist plötzlich viel größer</li>
-            <li>der Muskelkater bleibt aus</li>
-        </ul>
-        <p>Die Faszienübungen sind echte Allrounder!</p>
-    </section>
-    <a class="button" href="#">Jetzt zur Probe mitmachen</a>
-</article>
-<nav class="zapper">
-    <a class="zap previous" href="#dem-regen-trotzen-und-noch-länger">dem-regen-trotzen-und-noch-länger</a>
-    <a class="zap next" href="#den-rücken-verrücken">den-rücken-verrücken</a>
-</nav>
+    </header>
+    <article class="container-content">
+        <section class="member">
+            <blockquote class="mainquote"
+                        style="background-image: url('/images/faszien-rolle.jpg'); background-position: right, bottom;">
+                <span>
+                    "das müsste ich eigentlich viel häufiger machen!"
+                </span>
+            </blockquote>
+            <blockquote class="subquote">
+                "Ich spiele schon soo lange Volleyball, aber erst jetzt merke ich, was mir fehlt!"
+            </blockquote>
+        </section>
+        <section class="marion">
+            <blockquote class="floatedGroup">
+                <img class="photo floatLeft" src="/images/marion.jpg" alt="Trainerin Marion"/>
+                "Sowas höre ich öfter! Da ist jemand schon sehr sportlich und plötzlich ist da der Ah-Ha Effekt."
+            </blockquote>
+            <ul>
+                <li>die Beine erholen sich viel schneller,</li>
+                <li>der Bewegungsumfang der Arme ist plötzlich viel größer</li>
+                <li>der Muskelkater bleibt aus</li>
+            </ul>
+            <p>Die Faszienübungen sind echte Allrounder!</p>
+        </section>
+        <a class="button" href="#">Jetzt zur Probe mitmachen</a>
+    </article>
+    <nav class="zapper">
+        <a class="zap previous" href="#dem-regen-trotzen-und-noch-länger">dem-regen-trotzen-und-noch-länger</a>
+        <a class="zap next" href="#den-rücken-verrücken">den-rücken-verrücken</a>
+    </nav>
+</div>
 </body>
 </html>
