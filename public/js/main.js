d1024bbb699613ac93d3299ff71cacb050ec7dba
Reinhard Westermann <reinhard.westermann@kartenmacherei.de>
2020-07-04 11:20:53 +0200

    move logic into private methods

--- public/js/main.js
+++ public/js/main.js
@@ -17,26 +17,46 @@ function ready(callbackFunc) {
 
 ready(function () {
     const toggleFlyout = function (e) {
-        const flyoutWrapper = e.currentTarget.closest('.jsFlyoutWrapper');
+        const flyoutWrapper = _getFlyoutWrapper(e);
         if (flyoutWrapper.classList.contains('open')) {
-            closeFlyout(flyoutWrapper);
+            _setClosed(flyoutWrapper);
         } else {
-            openFlyout(flyoutWrapper);
+            _setOpen(flyoutWrapper);
         }
 
         e.preventDefault();
         e.stopPropagation();
     };
 
-    const openFlyout = function (el) {
+    // const openFlyout = function (e) {
+    //     const flyoutWrapper = _getFlyoutWrapper(e);
+    //     _setOpen(flyoutWrapper);
+    //     e.preventDefault();
+    //     e.stopPropagation();
+    // };
+    //
+    // const closeFlyout = function (e) {
+    //     const flyoutWrapper = _getFlyoutWrapper(e);
+    //     _setClosed(flyoutWrapper);
+    //     e.preventDefault();
+    //     e.stopPropagation();
+    // };
+
+    const _setOpen = function (el) {
         el.classList.replace('closed', 'open');
     };
 
-    const closeFlyout = function (el) {
+    const _setClosed = function (el) {
         el.classList.replace('open', 'closed');
     };
 
+    const _getFlyoutWrapper = function(e) {
+        return e.currentTarget.closest('.jsFlyoutWrapper');
+    };
+
     [].forEach.call(document.querySelectorAll('.jsFlyoutToggle'), function (el) {
-        el.addEventListener('click', toggleFlyout)
+        el.addEventListener('click', toggleFlyout);
+        // el.addEventListener('mouseenter', openFlyout);
+        // el.addEventListener('mouseleave', closeFlyout);
     });
 });
