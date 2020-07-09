dc2a155d3b303d5d19ced7467d0b2bbee4e0814d
Reinhard Westermann <reinhard.westermann@kartenmacherei.de>
2020-07-09 16:37:16 +0200

    delete test folder

--- public/den-rücken-verrücken/index.php
+++ public/den-rücken-verrücken/index.php
@@ -6,5 +6,5 @@ use MitMarion\TemplateVariables\DenRueckenVerrueckenTemplateVariables;
 
 require_once __DIR__ . '/../../bootstrap.php';
 
-$test = new DenRueckenVerrueckenTemplateVariables();
-echo $factory->createStoryPage($test)->asString();
+$templateVariables = new DenRueckenVerrueckenTemplateVariables();
+echo $factory->createStoryPage($templateVariables)->asString();
--- public/die-fazien-fetzen/index.php
+++ public/die-fazien-fetzen/index.php
@@ -6,5 +6,5 @@ use MitMarion\TemplateVariables\DieFazienFetzenTemplateVariables;
 
 require_once __DIR__ . '/../../bootstrap.php';
 
-$test = new DieFazienFetzenTemplateVariables();
-echo $factory->createStoryPage($test)->asString();
+$templateVariables = new DieFazienFetzenTemplateVariables();
+echo $factory->createStoryPage($templateVariables)->asString();
