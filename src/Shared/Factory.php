ae2711b01b5b87be12dc305fe3fe0b3d485df822
Reinhard Westermann <reinhard.westermann@kartenmacherei.de>
2020-07-11 14:10:32 +0200

    add debug extension to Twig

--- src/Shared/Factory.php
+++ src/Shared/Factory.php
@@ -8,14 +8,21 @@ use Twig\Loader\FilesystemLoader;
 
 class Factory
 {
+    private const TWIG_DEBUG = true;
+
     public function createTwigEnvironment(string $templateRootPath, string $templateCachePath): Environment
     {
         $loader = new FilesystemLoader($templateRootPath);
 
-        return new Environment(
+        $twig = new Environment(
             $loader, [
                 'cache' => $templateCachePath,
             ]
         );
+        if (self::TWIG_DEBUG) {
+            $twig->addExtension(new \Twig\Extension\DebugExtension());
+        }
+
+        return $twig;
     }
 }
