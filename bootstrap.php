6027a5ac46802b0797e1061dd7b121bd8789ea13
Reinhard Westermann <reinhard.westermann@kartenmacherei.de>
2020-09-21 08:39:03 +0200

    do not display errors on production

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,6 @@
 
 declare(strict_types=1);
 error_reporting(E_ALL);
-ini_set('display_errors', '1');
+ini_set('display_errors', '0');
 require_once __DIR__ . '/vendor/autoload.php';
 $factory = new MitMarion\Factory(new \Shared\Factory());
